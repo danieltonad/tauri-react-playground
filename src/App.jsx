8f5b8176d17e6db53698333c1b3a6f6249a6481b
danieltonad <soladan2000@gmail.com>
2024-03-26 10:50:30 -0900

    dialog and alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-
+import { ask } from '@tauri-apps/api/dialog';
 const { invoke } = window.__TAURI__.tauri
 
 
 function App() {
   const [count, setCount] = useState(0);
-
+  
   const increment = () => {
     setCount(count + 1);
   }
-
-  const double_click = () => {
+  
+  const double_click = async () => {
+    const yes = await ask('Are you sure?', 'Tauri');
+    const yes2 = await ask('This action cannot be reverted. Are you sure?', { title: 'Tauri', type: 'warning' });
     setCount(count + 2);
   }
   
@@ -30,6 +32,7 @@ function App() {
           Pussy Nigga
         </p>
         <button onDoubleClick={double_click} onClick={increment}> click </button>
+        <button onClick={() => { alert(100) }}>  Show Dialog </button>
       </header>
     </div>
   );
